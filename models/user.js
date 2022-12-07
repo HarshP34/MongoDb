1ae6ddc33fcc54fe54dcca701f21e519fc213e3d
HarshP34 <hupatel1005@gmail.com>
2022-12-07 22:12:08 +0530

    mongoDB task-6

--- models/user.js
+++ models/user.js
@@ -79,7 +79,7 @@ return i.productId.toString()===p._id.toString();
    .catch(err=>console.log(err))
   }
 
-   deleteCartItem(prodId)
+   deleteItemFromCart(prodId)
   {
     
     const cartitems=this.cart.items;
@@ -89,6 +89,36 @@ return i.productId.toString()===p._id.toString();
     })
     const db=getDb();
     return db.collection('users').updateOne({_id:new ObjectId(this._id)},{$set:{cart:{items:updatedCart}}})
+  }
+
+
+  addOrder(){
+    const db=getDb();
+    return this.getCart().then(products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new ObjectId(this._id),
+          name:this.name
+        }
+      }
+      return   db.collection('orders').insertOne(order)
+    })
+ .then(result=>{
+      this.cart={items:[]};
+      return db
+      .collection('users')
+      .updateOne({_id:new ObjectId(this._id)},
+      {$set:{cart: { items: [] } } 
+    })
+    })
+  }
+
+  getOrders()
+  {
+    const db=getDb();
+    return db.collection('orders').find({'user._id':new ObjectId(this._id)})
+    .toArray()
   }
 
 }
