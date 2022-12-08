db13d0852bc1a354bfc12a25185545dad042b00c
HarshP34 <hupatel1005@gmail.com>
2022-12-08 10:07:35 +0530

    MongoDB task-8

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,10 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product=new Product(title,price,description,imageUrl,null,req.user._id);
+  const product=new Product({title:title,
+    price:price,
+    description:description,
+    imageUrl:imageUrl});
  product.save()
     .then(result => {
       console.log(result);
@@ -34,7 +37,6 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-
    Product.findById(prodId)
    .then(product => {
       if (!product) {
@@ -57,8 +59,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
  
-    const product=new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl,prodId)
-    product.save()
+    Product.findById(prodId).then(product=>{
+      product.title=updatedTitle;
+      product.price=updatedPrice;
+      product.description=updatedDesc;
+      product.imageUrl=updatedImageUrl;
+      return product.save()
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -67,7 +74,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
- Product.fetchAll()
+ Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -80,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(result => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
